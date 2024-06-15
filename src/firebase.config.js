30eb455879c46e8f233232c0154a6a6a9031f38c
Maera <baeitsmae@gmail.com>
2024-06-15 08:26:03 +0200

    test

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -9,11 +9,10 @@ import { getFirestore } from 'firebase/firestore'
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY, 
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: "dsh-app",
+  projectId: "dsh-tracking",
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-  measurementId : process.env.REACT_APP_MEASUREMENT_ID
 }
 
 // Initialise l'app Firebase 
